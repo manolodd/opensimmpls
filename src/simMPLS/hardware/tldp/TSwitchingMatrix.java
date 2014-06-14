9ef11981f946383f6d68dc1bd47bfa30db557f58
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2014-06-14 19:44:51 +0200

    Translation is going on.

--- src/simMPLS/hardware/tldp/TSwitchingMatrix.java
+++ src/simMPLS/hardware/tldp/TSwitchingMatrix.java
@@ -43,8 +43,8 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo devuelve el monitor de la clase, que permitir� realizar
- operaciones sobre la switchingMatrix de conmutaci�n sin peligro de accesos
- concurrentes mal llevados a cabo.
+     * operaciones sobre la switchingMatrix de conmutaci�n sin peligro de
+     * accesos concurrentes mal llevados a cabo.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @return El monitor que hace de monitor para operar en esta clase.
@@ -56,7 +56,7 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo vac�a todas las entradas existentes en la switchingMatrix de
- conmutaci�n, dej�ndola inicializada como antes del proceso de simulaci�n.
+     * conmutaci�n, dej�ndola inicializada como antes del proceso de simulaci�n.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 1.0
@@ -72,11 +72,12 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo inserta una nueva entrada en la switchingMatrix de conmutaci�n.
+     * Este m�todo inserta una nueva entrada en la switchingMatrix de
+     * conmutaci�n.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param switchingMatrixEntry La nueva entrada que deseamos addEntry en la switchingMatrix de
- conmutaci�n.
+     * @param switchingMatrixEntry La nueva entrada que deseamos addEntry en la
+     * switchingMatrix de conmutaci�n.
      * @since 1.0
      */
     public void addEntry(TSwitchingMatrixEntry switchingMatrixEntry) {
@@ -86,16 +87,18 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo permite acceder a una entrada concreta de la switchingMatrix de
- conmutaci�n.
+     * Este m�todo permite acceder a una entrada concreta de la switchingMatrix
+     * de conmutaci�n.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param incomingPortID El puerto de la entrada de la switchingMatrix que buscamos.
-     * @param labelOrFEC El valor de la labelAux de entrada o del campo FEC_ENTRY de la
- entrada de la switchingMatrix que buscamos.
+     * @param incomingPortID El puerto de la entrada de la switchingMatrix que
+     * buscamos.
+     * @param labelOrFEC El valor de la labelAux de entrada o del campo
+     * FEC_ENTRY de la entrada de la switchingMatrix que buscamos.
      * @param entryType El tipo de entrada (ILM o FTN) que estamos buscando.
-     * @return Devuelve la entrada de la switchingMatrix de conmutaci�n que concuerda con
- los parametros de b�squeda, si la encuentra, o null, en caso contrario.
+     * @return Devuelve la entrada de la switchingMatrix de conmutaci�n que
+     * concuerda con los parametros de b�squeda, si la encuentra, o null, en
+     * caso contrario.
      * @since 1.0
      */
     public TSwitchingMatrixEntry getEntry(int incomingPortID, int labelOrFEC, int entryType) {
@@ -118,15 +121,15 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo permite acceder a una entrada concreta de la switchingMatrix de
- conmutaci�n, que debe tener un identificador de sesi�n TLDP propio
- ex�ctamente igua al indicado.
+     * Este m�todo permite acceder a una entrada concreta de la switchingMatrix
+     * de conmutaci�n, que debe tener un identificador de sesi�n TLDP propio
+     * ex�ctamente igua al indicado.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param localTLDPSessionID Identificador TLDP propio que asignamos en su momento a la
- entrada de la switchingMatrix de conmutaci�n que buscamos.
-     * @return La entrada de la switchingMatrix de conmutaci�n coincidente, en caso de
- encontrarla, y null en caso contrario.
+     * @param localTLDPSessionID Identificador TLDP propio que asignamos en su
+     * momento a la entrada de la switchingMatrix de conmutaci�n que buscamos.
+     * @return La entrada de la switchingMatrix de conmutaci�n coincidente, en
+     * caso de encontrarla, y null en caso contrario.
      * @since 1.0
      */
     public TSwitchingMatrixEntry getEntryHavingLocalTLDPSessionID(int localTLDPSessionID) {
@@ -149,14 +152,14 @@ public class TSwitchingMatrix {
      * nodo antecesor concreto.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param upstreamTLDPSessionID El identificador de sesi�n TLDP que el nodo antecesor
-     * especifico para la entrada que estamos buscando.
+     * @param upstreamTLDPSessionID El identificador de sesi�n TLDP que el nodo
+     * antecesor especifico para la entrada que estamos buscando.
      * @param incomingPortID El puerto de entrada asociado a la entrada TLDP que
      * estamos buscando. Necesario porque s�lo con el identificador TLDP del
      * antecesor no vale ya que distintos antecesores pueden otorgar el mismo
      * identificador de sesi�n TLDP.
-     * @return La entrada de la switchingMatrix de conmutaci�n buscada, si la encuentra,
- o null en caso contrario.
+     * @return La entrada de la switchingMatrix de conmutaci�n buscada, si la
+     * encuentra, o null en caso contrario.
      * @since 1.0
      */
     public TSwitchingMatrixEntry getEntryHavinUpstreamTLDPSessionID(int upstreamTLDPSessionID, int incomingPortID) {
@@ -178,15 +181,16 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo sirve para ver si hay un acierto al buscar una entrada de la
- switchingMatrix de conmutaci�n concreta o no.
+     * switchingMatrix de conmutaci�n concreta o no.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param incomingPortID El puerto de la entrada de la switchingMatrix que buscamos.
-     * @param labelOrFEC El valor de la labelAux de entrada o del campo FEC_ENTRY de la
- entrada de la switchingMatrix que buscamos.
+     * @param incomingPortID El puerto de la entrada de la switchingMatrix que
+     * buscamos.
+     * @param labelOrFEC El valor de la labelAux de entrada o del campo
+     * FEC_ENTRY de la entrada de la switchingMatrix que buscamos.
      * @param entryType El tipo de entrada (ILM o FTN) que estamos buscando.
-     * @return Devuelve true si existe una entrada coincidente en la switchingMatrix de
- conmutaci�n y false en caso contrario.
+     * @return Devuelve true si existe una entrada coincidente en la
+     * switchingMatrix de conmutaci�n y false en caso contrario.
      * @since 1.0
      */
     public boolean existsEntry(int incomingPortID, int labelOrFEC, int entryType) {
@@ -209,14 +213,16 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo elimina una entrada concreta de la switchingMatrix de conmutaci�n.
+     * Este m�todo elimina una entrada concreta de la switchingMatrix de
+     * conmutaci�n.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param incomingPortID El puerto de la entrada de la switchingMatrix que queremos borrar.
-     * @param labelOrFEC El valor de la labelAux de entrada o del campo FEC_ENTRY de la
- entrada de la switchingMatrix que queremos borrar.
-     * @param entryType El tipo de entrada (ILM o FTN) de la entrada de la switchingMatrix que
- deseamos borrar.
+     * @param incomingPortID El puerto de la entrada de la switchingMatrix que
+     * queremos borrar.
+     * @param labelOrFEC El valor de la labelAux de entrada o del campo
+     * FEC_ENTRY de la entrada de la switchingMatrix que queremos borrar.
+     * @param entryType El tipo de entrada (ILM o FTN) de la entrada de la
+     * switchingMatrix que deseamos borrar.
      * @since 1.0
      */
     public void removeEntry(int incomingPortID, int labelOrFEC, int entryType) {
@@ -237,13 +243,14 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo elimina una entrada concreta de la switchingMatrix de conmutaci�n.
+     * Este m�todo elimina una entrada concreta de la switchingMatrix de
+     * conmutaci�n.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param localTLDPSessionID El identificador TLDP propio de la entrada de la switchingMatrix que
- deseamos eliminar.
-     * @param incomingPortID El puerto de entrada de la entrada de la switchingMatrix de
- conmutaci�n que queremos eliminar.
+     * @param localTLDPSessionID El identificador TLDP propio de la entrada de
+     * la switchingMatrix que deseamos eliminar.
+     * @param incomingPortID El puerto de entrada de la entrada de la
+     * switchingMatrix de conmutaci�n que queremos eliminar.
      * @since 1.0
      */
     public void removeEntryHavingLocalTLDPSessionID(int localTLDPSessionID, int incomingPortID) {
@@ -263,17 +270,18 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo obtiene la operaci�n que hay que realizar sobre la pila de
- etiquetas de los paquetes de un flujo concreto, seg�n la entrada de la
- switchingMatrix de conmutaci�n asociada a ese flujo.
+     * etiquetas de los paquetes de un flujo concreto, seg�n la entrada de la
+     * switchingMatrix de conmutaci�n asociada a ese flujo.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param incomingPortID El puerto de la entrada de la switchingMatrix que queremos consultar.
-     * @param labelOrFEC El valor de la labelAux de entrada o del campo FEC_ENTRY de la
- entrada de la switchingMatrix que queremos consultar.
-     * @param entryType El tipo de entrada (ILM o FTN) de la entrada que queremos
-     * consultar.
+     * @param incomingPortID El puerto de la entrada de la switchingMatrix que
+     * queremos consultar.
+     * @param labelOrFEC El valor de la labelAux de entrada o del campo
+     * FEC_ENTRY de la entrada de la switchingMatrix que queremos consultar.
+     * @param entryType El tipo de entrada (ILM o FTN) de la entrada que
+     * queremos consultar.
      * @return Devuelve la operaci�n a realizar sobre la pila de etiquetas. Es
- una de las constantes definidas en TSwitchingMatrixEntry.
+     * una de las constantes definidas en TSwitchingMatrixEntry.
      * @since 1.0
      */
     public int getLabelStackOperation(int incomingPortID, int labelOrFEC, int entryType) {
@@ -297,17 +305,18 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo obtiene la labelAux de salida para los paquetes de un flujo
- concreto, seg�n la entrada de la switchingMatrix de conmutaci�n asociada a ese
- flujo.
+     * concreto, seg�n la entrada de la switchingMatrix de conmutaci�n asociada
+     * a ese flujo.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param incomingPortID El puerto de la entrada de la switchingMatrix que queremos consultar.
-     * @param labelOrFEC El valor de la labelAux de entrada o del campo FEC_ENTRY de la
- entrada de la switchingMatrix que queremos consultar.
-     * @param entryType El tipo de entrada (ILM o FTN) de la entrada de la switchingMatrix que
- queremos consultar.
+     * @param incomingPortID El puerto de la entrada de la switchingMatrix que
+     * queremos consultar.
+     * @param labelOrFEC El valor de la labelAux de entrada o del campo
+     * FEC_ENTRY de la entrada de la switchingMatrix que queremos consultar.
+     * @param entryType El tipo de entrada (ILM o FTN) de la entrada de la
+     * switchingMatrix que queremos consultar.
      * @return La labelAux de salida que hay que poner a los paquetes del flujo
- asociado a la entrada de la switchingMatrix que hemos especificado.
+     * asociado a la entrada de la switchingMatrix que hemos especificado.
      * @since 1.0
      */
     public int getOutgoingLabel(int incomingPortID, int labelOrFEC, int entryType) {
@@ -331,17 +340,18 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo obtiene el puerto de salida de los paquetes de un flujo
- concreto, seg�n la entrada de la switchingMatrix de conmutaci�n asociada a ese
- flujo.
+     * concreto, seg�n la entrada de la switchingMatrix de conmutaci�n asociada
+     * a ese flujo.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param incomingPortID El puerto de la entrada de la switchingMatrix que quermos consultar.
-     * @param labelOrFEC El valor de la labelAux de entrada o del campo FEC_ENTRY de la
- entrada de la switchingMatrix que queremos consultar.
-     * @param entryType El tipo de entrada (ILM o FTN) de la entrada de la switchingMatrix que
- queremos consultar.
+     * @param incomingPortID El puerto de la entrada de la switchingMatrix que
+     * quermos consultar.
+     * @param labelOrFEC El valor de la labelAux de entrada o del campo
+     * FEC_ENTRY de la entrada de la switchingMatrix que queremos consultar.
+     * @param entryType El tipo de entrada (ILM o FTN) de la entrada de la
+     * switchingMatrix que queremos consultar.
      * @return El puerto de salida para los paquetes del flujo asociado a la
- entrada de la switchingMatrix especificada.
+     * entrada de la switchingMatrix especificada.
      * @since 1.0
      */
     public int getOutgoingPortID(int incomingPortID, int labelOrFEC, int entryType) {
@@ -365,13 +375,13 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo comprueba si hay alguna entrada cuya labelAux de entrada
- coincide con la epecificada.
+     * coincide con la epecificada.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @param label Etiqueta que debe tener la entrada de la switchingMatrix que estamos
- buscando.
-     * @return true si existe una entrada de la switchingMatrix con esa labelAux de
- entrada, si no, false.
+     * @param label Etiqueta que debe tener la entrada de la switchingMatrix que
+     * estamos buscando.
+     * @return true si existe una entrada de la switchingMatrix con esa labelAux
+     * de entrada, si no, false.
      * @since 1.0
      */
     public boolean existsEntryHavingLabel(int label) {
@@ -393,12 +403,12 @@ public class TSwitchingMatrix {
 
     /**
      * Este m�todo devuelve una labelAux de 20 bits �nica. Generalmente para
- responder a una petici�n de labelAux realizada por otro nodo antecesor.
+     * responder a una petici�n de labelAux realizada por otro nodo antecesor.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @return Una labelAux para recibir tr�fico, si es posible, o la constante
- ETIQUETA_NO_DISPONIBLE, en caso de que se haya agotado el espacio de
- etiquetas del nodo.
+     * ETIQUETA_NO_DISPONIBLE, en caso de que se haya agotado el espacio de
+     * etiquetas del nodo.
      * @since 1.0
      */
     public int getNewLabel() {
@@ -419,7 +429,7 @@ public class TSwitchingMatrix {
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @return El iterador de la lista enlazada que contiene las entradas de la
- switchingMatrix de conmutaci�n.
+     * switchingMatrix de conmutaci�n.
      * @since 1.0
      */
     public Iterator getEntriesIterator() {
@@ -427,8 +437,8 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo obtiene el n�mero de entradas que hay en la switchingMatrix de
- conmutaci�n.
+     * Este m�todo obtiene el n�mero de entradas que hay en la switchingMatrix
+     * de conmutaci�n.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @return N�mero de entradas de la switchingMatrix de conmutacion.
@@ -439,8 +449,8 @@ public class TSwitchingMatrix {
     }
 
     /**
-     * Este m�todo reinicia la switchingMatrix de conmutaci�n y la deja como si acabase
- de ser creada por el constructor de la clase.
+     * Este m�todo reinicia la switchingMatrix de conmutaci�n y la deja como si
+     * acabase de ser creada por el constructor de la clase.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 1.0
