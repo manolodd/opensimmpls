942b65b68471a08b8f9d6a24b012419201fee94d
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2018-05-07 18:14:39 +0200

    TSimulationEventLSPRemoved has been translated and refactored

--- src/com/manolodominguez/opensimmpls/scenario/simulationevents/TSimulationEventLSPRemoved.java
+++ src/com/manolodominguez/opensimmpls/scenario/simulationevents/TSimulationEventLSPRemoved.java
@@ -15,10 +15,6 @@
  */
 package com.manolodominguez.opensimmpls.scenario.simulationevents;
 
-import com.manolodominguez.opensimmpls.scenario.TLink;
-import com.manolodominguez.opensimmpls.scenario.TNode;
-import com.manolodominguez.opensimmpls.scenario.TTopologyElement;
-
 /**
  * This class implements a simulation event that is generated when a new LSP is
  * removed.
@@ -54,51 +50,6 @@ public class TSimulationEventLSPRemoved extends TSimulationEvent {
     @Override
     public int getSubtype() {
         return TSimulationEvent.LSP_REMOVED;
-    }
-
-    /**
-     * This method gets the name of the topology element that generated the
-     * event.
-     *
-     * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @return the name of the topology element that generated the event.
-     * @since 2.0
-     */
-    public String getSourceName() {
-        TTopologyElement topologyElement = null;
-        TLink link = null;
-        TNode node = null;
-        topologyElement = super.getSource();
-        if (topologyElement.getElementType() == TTopologyElement.LINK) {
-            link = (TLink) topologyElement;
-            return link.getName();
-        } else if (topologyElement.getElementType() == TTopologyElement.NODE) {
-            node = (TNode) topologyElement;
-            return node.getName();
-        }
-        return ("");
-    }
-
-    /**
-     * This method gets the type of the topology element that generated the
-     * event, as a String.
-     *
-     * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
-     * @return the type of the topology element that generated the event, as a
-     * String.
-     * @since 2.0
-     */
-    public String getSourceTypeAsString() {
-        TTopologyElement topologyElement = null;
-        topologyElement = super.getSource();
-        if (topologyElement.getElementType() == TTopologyElement.LINK) {
-            // FIX: i18N needed
-            return ("Enlace ");
-        } else if (topologyElement.getElementType() == TTopologyElement.NODE) {
-            // FIX: i18N needed
-            return ("Nodo ");
-        }
-        return ("");
     }
 
     /**
