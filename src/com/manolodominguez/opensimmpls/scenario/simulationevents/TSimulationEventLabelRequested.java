ee7284352c13c20750df0b9b916184967c2366ed
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2018-05-07 18:28:21 +0200

    TSimulationEventLabelRequested has been translated and refactored

--- src/com/manolodominguez/opensimmpls/scenario/simulationevents/TSimulationEventLabelRequested.java
+++ src/com/manolodominguez/opensimmpls/scenario/simulationevents/TSimulationEventLabelRequested.java
@@ -26,7 +26,7 @@ public class TSimulationEventLabelRequested extends TSimulationEvent {
 
     /**
      * This method is the constructor of the class. It creates a new instance of
-     * TSimulationEventLabelDenied.
+     * TSimulationEventLabelRequested.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @param source The object that generates the event.
