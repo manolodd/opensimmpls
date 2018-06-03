bc6cf1bccbf155c191c4951ae992f75ef8bdb101
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2018-06-03 21:23:32 +0200

    Added to allow a correct size inside JScrollPane.

---
+++ src/com/manolodominguez/opensimmpls/ui/utils/JScrollablePanel.java
@@ -0,0 +1,57 @@
+/* 
+ * Copyright (C) Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.manolodominguez.opensimmpls.ui.utils;
+
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+import javax.swing.Scrollable;
+
+public class JScrollablePanel extends JPanel implements Scrollable {
+
+    public JScrollablePanel(LayoutManager layout) {
+        super(layout);
+    }
+
+    public JScrollablePanel() {
+    }
+
+    @Override
+    public Dimension getPreferredScrollableViewportSize() {
+        return super.getPreferredSize();
+    }
+
+    @Override
+    public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+        return 50;
+    }
+
+    @Override
+    public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+        return 50;
+    }
+
+    @Override
+    public boolean getScrollableTracksViewportWidth() {
+        return true;
+    }
+
+    @Override
+    public boolean getScrollableTracksViewportHeight() {
+        return false;
+    }
+}
