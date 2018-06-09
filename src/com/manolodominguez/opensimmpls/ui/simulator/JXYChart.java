fcdab672039ab4eb3f98ae3eda8c48ed9722c84c
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2018-06-09 14:56:02 +0200

    JXYChart and JBarChart are being translated and refactored.

--- src/com/manolodominguez/opensimmpls/ui/simulator/JXYChart.java
+++ src/com/manolodominguez/opensimmpls/ui/simulator/JXYChart.java
@@ -33,42 +33,42 @@ public class JXYChart {
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
         renderer.setDefaultShapesVisible(false);
         renderer.setSeriesPaint(0, Color.RED);
-        renderer.setSeriesStroke(0, new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(0, new BasicStroke(STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         renderer.setSeriesPaint(1, Color.BLUE);
-        renderer.setSeriesStroke(1, new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(1, new BasicStroke(STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         renderer.setSeriesPaint(2, Color.GREEN);
-        renderer.setSeriesStroke(1, new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(2, new BasicStroke(STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         renderer.setSeriesPaint(3, Color.MAGENTA);
-        renderer.setSeriesStroke(1, new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(3, new BasicStroke(STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         renderer.setSeriesPaint(4, Color.ORANGE);
-        renderer.setSeriesStroke(1, new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(4, new BasicStroke(STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         renderer.setSeriesPaint(5, Color.BLACK);
-        renderer.setSeriesStroke(1, new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(5, new BasicStroke(STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         this.plot.setRenderer(renderer);
-        this.plot.setBackgroundPaint(Color.white);
+        this.plot.setBackgroundPaint(Color.WHITE);
         this.plot.setRangeGridlinesVisible(true);
         this.plot.setRangeGridlinePaint(Color.DARK_GRAY);
         this.plot.setDomainGridlinesVisible(true);
         this.plot.setDomainGridlinePaint(Color.DARK_GRAY);
         this.chart.getLegend().setFrame(BlockBorder.NONE);
-        this.chart.setTitle(new TextTitle(chartTitle, new Font("Serif", Font.BOLD, 18)));
+        this.chart.setTitle(new TextTitle(chartTitle, TITLE_FONT));
         this.chartPanel = new ChartPanel(this.chart, false, true, false, false, false);
-        this.chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-        this.chartPanel.setBackground(Color.white);
+        this.chartPanel.setBorder(BorderFactory.createEmptyBorder(BORDER_PADDING, BORDER_PADDING, BORDER_PADDING, BORDER_PADDING));
+        this.chartPanel.setBackground(Color.WHITE);
         this.chartPanel.setDomainZoomable(false);
         this.chartPanel.setRangeZoomable(false);
     }
 
-    public JFreeChart getChart() {
-        return this.chart;
-    }
-    
     public ChartPanel getChartPanel() {
         return this.chartPanel;
     }
 
-    XYDataset series;
-    JFreeChart chart;
-    ChartPanel chartPanel;
-    XYPlot plot;
+    private static final float STROKE_WIDTH = 1.0f;
+    private static final int BORDER_PADDING = 15;
+    private static final Font TITLE_FONT = new Font("Serif", Font.BOLD, 18);
+
+    private XYDataset series;
+    private JFreeChart chart;
+    private ChartPanel chartPanel;
+    private XYPlot plot;
 }
