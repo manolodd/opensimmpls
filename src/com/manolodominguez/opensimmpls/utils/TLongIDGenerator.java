bde5fd2329960812d151d7b40cddf9e22dde9830
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2018-06-15 21:15:07 +0200

    TLongIDGenerator has been translated and refactored.

--- src/com/manolodominguez/opensimmpls/utils/TLongIDGenerator.java
+++ src/com/manolodominguez/opensimmpls/utils/TLongIDGenerator.java
@@ -15,52 +15,55 @@
  */
 package com.manolodominguez.opensimmpls.utils;
 
-/** Esta es la implementaci�n de un generador de identificadores num�ricos que no se
- * repiten y que se van incrementando hasta llegar a su l�mite m�ximo.
- * @version 1.0
- * @author <B>Manuel Dom�nguez Dorado</B><br><A
- * href="mailto:ingeniero@ManoloDominguez.com">ingeniero@ManoloDominguez.com</A><br><A href="http://www.ManoloDominguez.com" target="_blank">http://www.ManoloDominguez.com</A>
+/**
+ * This class implements a ID generator that generates numeric IDs that are
+ * always different, in a cycle.
+ *
+ * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
+ * @version 2.0
  */
 public class TLongIDGenerator {
-    
-    /** Atributo que almacena el valor interno del generador de identificadores
-     * num�ticos largos.
-     * @since 2.0
-     */
-    private long identificador;
-    
-    /** Crea un nuevo generador de identificadores con el valor inicial 0.
+
+    /**
+     * This method is the constructor of the class. It is create a new instance
+     * of TLongIDGenerator.
+     *
+     * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 2.0
      */
     public TLongIDGenerator() {
-        identificador = 0;
+        // FIX: Do not use harcoded values. Use class constants instead.
+        this.identifier = 0;
     }
-    
+
     /**
-     * Este m�todo reinicia el generador de identificadores a su valor original, como
-     * si acabase de ser instanciado.
+     * This method resets the ID generator to its initial internal value.
+     *
+     * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 2.0
      */
     public synchronized void reset() {
-        identificador = 0;
+        // FIX: Do not use harcoded values. Use class constants instead.
+        this.identifier = 0;
     }
-    
-    /** M�todo que devuelve un identificador generado por el generador. Adem�s se
-     * modifica el contador interno para que el siguiente generador sea distinto. El
-     * m�todo est� <B>sincronizado</B>.
-     * @return Un n�mero enterio que ser� un identificador largo: un entero largo �nico.
-     * @throws EIDGeneratorOverflow Esta excepci�n se lanza cuando el contador interno del generador de
-     * identificadores se desborda. Es alto por lo que generalmente no ocurrir�, pero
-     * hay que capturar la excepci�n por si acaso.
+
+    /**
+     * This method generates a new ID.
+     *
+     * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
+     * @return a long value that is unique.
+     * @throws EIDGeneratorOverflow when the ID generator reaches its maximum
+     * value.
      * @since 2.0
      */
-    synchronized public long getNextID() throws EIDGeneratorOverflow {
-        if (identificador > 9223372036854775806L) {
+    synchronized public long getNextIdentifier() throws EIDGeneratorOverflow {
+        if (this.identifier >= Long.MAX_VALUE) {
             throw new EIDGeneratorOverflow();
         } else {
-            identificador++;
+            this.identifier++;
         }
-        return (identificador);
+        return (this.identifier);
     }
-    
+
+    private long identifier;
 }
