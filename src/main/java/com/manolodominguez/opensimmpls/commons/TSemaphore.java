509f78fbb9b673bd64095ec339f5403b03806cb6
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2019-04-20 19:38:04 +0200

    Name changed from TLock to TSemaphore that is more intuitive

--- src/main/java/com/manolodominguez/opensimmpls/commons/TSemaphore.java
+++ src/main/java/com/manolodominguez/opensimmpls/commons/TSemaphore.java
@@ -19,9 +19,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class implements a generic monitor that can be used to limit the number
- * of threads that can acces a piece of code simultaneously. That's useful in
- * multithreading environments.
+ * This class implements a generic semaphore that can be used to limit the
+ * number of threads that can acces a piece of code simultaneously. That's
+ * useful in multithreading environments.
  *
  * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
  * @version 2.0
@@ -30,52 +30,52 @@ public class TSemaphore {
 
     /**
      * This method is the constructor of the class. It is create a new instance
-     * of TLock.
+     * of TSemaphore.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 2.0
      */
     public TSemaphore() {
-        this.lock = UNLOCKED;
+        this.semaphore = GREEN;
     }
 
     /**
      * When this method is called from a thread, the thread is blocked if there
      * is a thread that has previously called the method, until that thread
-     * releases the lock. If no thread has the monitor locked, then the calling
-     * thread takes control and blocks the rest until it releases the monitor.
+     * releases the semaphore. If no thread has locked the semaphore, then the
+     * calling thread takes control and blocks the rest until it releases the
+     * semaphore.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 2.0
      */
-    public synchronized void lock() {
-        while (this.lock == LOCKED) {
+    public synchronized void setRed() {
+        while (this.semaphore == RED) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 this.logger.error(e.getMessage(), e);
             }
         }
-        this.lock = LOCKED;
+        this.semaphore = RED;
     }
 
     /**
-     * When a thread calls this method, it releases the monitor. In this way,
-     * the next thread that would have called the lock() method and was blocked,
-     * will be unlocked.
+     * When a thread calls this method, it releases the semaphore. In this way,
+     * the next thread that called the setRed() method and was blocked, will be
+     * released.
      *
      * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
      * @since 2.0
      */
-    public synchronized void unLock() {
-        this.lock = UNLOCKED;
+    public synchronized void setGreen() {
+        this.semaphore = GREEN;
         notify();
     }
 
-    private boolean lock;
-    
-    
-    private static final boolean LOCKED = true;
-    private static final boolean UNLOCKED = false;
+    private boolean semaphore;
+
+    private static final boolean RED = true;
+    private static final boolean GREEN = false;
     private final Logger logger = LoggerFactory.getLogger(TSemaphore.class);
 }
